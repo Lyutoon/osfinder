c444fb1f7248970e389cbdc4b6093e7b91b111b2
Tong Liu <57178900+Lyutoon@users.noreply.github.com>
2024-04-11 21:22:20 +0800

    Update command line of finder.py

--- finder.py
+++ finder.py
@@ -1,12 +1,12 @@
 import argparse
 
-def find_import_path(package, target_attr):
+def find_import_path(package, target_attr, maxl, maxn):
     exec(f'import {package}')
     stack = [(eval(package), package)]
     trace = []
     while stack:
         current_obj, import_path = stack.pop()
-        if len(import_path.split('.')) > 5:
+        if len(import_path.split('.')) > maxl:
             continue
         try:
             for attr_name in dir(current_obj):
@@ -17,7 +17,7 @@ def find_import_path(package, target_attr):
                 elif isinstance(attr, type(current_obj)):
                     module_path = f"{import_path}.{attr_name}"
                     stack.append((attr, module_path))
-                if len(trace) > 1000:
+                if len(trace) > maxn:
                     return trace
         except:
             pass
@@ -27,11 +27,15 @@ def find_import_path(package, target_attr):
 def main(args):
     package = args.package
     target_attr = args.target
-    import_path = find_import_path(package, target_attr)
+    maxl = args.maxl
+    maxn = args.maxn
+    import_path = find_import_path(package, target_attr, maxl, maxn)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--package', help='third party package', type=str)
     parser.add_argument('--target', help='target, default to be os', type=str, default='os')
+    parser.add_argument('--maxl', help='maximum length of the import chain', type=int, default=5)
+    parser.add_argument('--maxn', help='maximum number of the import chains', type=int, default=100)
     args = parser.parse_args()
     main(args)
